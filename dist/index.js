9495baf69cab3e5018dababc2100700a86a3d288
Chris Coppola <mynamereallysux@gmail.com>
2018-01-02 10:32:45 -0500

    Build Dist

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _ConsoleUtils = require('./ConsoleUtils');
+
+Object.defineProperty(exports, 'ConsoleUtils', {
+  enumerable: true,
+  get: function get() {
+    return _ConsoleUtils.ConsoleUtils;
+  }
+});
