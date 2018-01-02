d6408906dda0b122f8aeea1be37ec4e883601998
Chris Coppola <mynamereallysux@gmail.com>
2018-01-02 10:47:39 -0500

    Fixed exports

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,56 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 
 var _ConsoleUtils = require('./ConsoleUtils');
 
 Object.defineProperty(exports, 'ConsoleUtils', {
-  enumerable: true,
-  get: function get() {
-    return _ConsoleUtils.ConsoleUtils;
-  }
+    enumerable: true,
+    get: function get() {
+        return _ConsoleUtils.ConsoleUtils;
+    }
+});
+Object.defineProperty(exports, 'print', {
+    enumerable: true,
+    get: function get() {
+        return _ConsoleUtils.print;
+    }
+});
+Object.defineProperty(exports, 'info', {
+    enumerable: true,
+    get: function get() {
+        return _ConsoleUtils.info;
+    }
+});
+Object.defineProperty(exports, 'debug', {
+    enumerable: true,
+    get: function get() {
+        return _ConsoleUtils.debug;
+    }
+});
+Object.defineProperty(exports, 'warn', {
+    enumerable: true,
+    get: function get() {
+        return _ConsoleUtils.warn;
+    }
+});
+Object.defineProperty(exports, 'error', {
+    enumerable: true,
+    get: function get() {
+        return _ConsoleUtils.error;
+    }
+});
+Object.defineProperty(exports, 'supressConsole', {
+    enumerable: true,
+    get: function get() {
+        return _ConsoleUtils.supressConsole;
+    }
+});
+Object.defineProperty(exports, 'restoreConsole', {
+    enumerable: true,
+    get: function get() {
+        return _ConsoleUtils.restoreConsole;
+    }
 });
